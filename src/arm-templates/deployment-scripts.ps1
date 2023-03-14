bb2f4f541af3400206e05f38aa32e8d2a8d41daf
William Mba <contact@wgmconsulting.ca>
2023-03-14 06:49:42 -0400

    updated deployment scripts

--- src/arm-templates/deployment-scripts.ps1
+++ src/arm-templates/deployment-scripts.ps1
@@ -1,24 +1,29 @@
+param(
+    [string]$Environment
+)
 #1 Define the suscription to use for the deployment
-Write-Host('::::::::::::::::::::::: Deployment started :::::::::::::::::::::::')
+Write-Host("::::::::::::::::::::::: Deployment started in $Environment :::::::::::::::::::::::")
 
-Write-Host('Setting account subscription...')
+Write-Host("Setting account subscription for $Environment ...")
 az account set -s "Az Subscription Main"
 Write-Host('Subscription setted.')
+Write-Host
 
 #2 Deploy the resource group
-Write-Host('Deploying resource group...')
+Write-Host("Deploying resource group to $Environment ...")
 $rgName=(az deployment sub create `
     --location "canadacentral" `
     --template-file "rg-azure-deploy.json" `
-    --parameters "rg-azure-deploy.parameters.test.json" | ConvertFrom-Json).properties.parameters.rgName
+    --parameters "rg-azure-deploy.parameters.$Environment.json" | ConvertFrom-Json).properties.parameters.rgName
 Write-Host('Resource group deployed.')
+Write-Host
 
 #3 Deploy resources
-Write-Host('Deploying resources...')
+Write-Host("Deploying resources to $Environment environment ...")
 az deployment group create `
     --resource-group $rgName.value `
     --template-file "azure-deploy.json" `
-    --parameters "azure-deploy.parameters.test.json"
-Write-Host('Resources deployed.')
-Write-Host
-Write-Host('::::::::::::::::::::::: Deployment completed successfully :::::::::::::::::::::::')
+    --parameters "azure-deploy.parameters.$Environment.json"
+
+##TODO Put this message in an if statement
+Write-Host("::::::::::::::::::::::: Resources successfully deployed to $Environment :::::::::::::::::::::::")
