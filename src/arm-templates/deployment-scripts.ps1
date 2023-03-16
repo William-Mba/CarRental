b209cfdbcc90758106db29772d95e6c8d07e2dc6
William Mba <contact@wgmconsulting.ca>
2023-03-16 06:00:27 -0400

    update arm templates

--- src/arm-templates/deployment-scripts.ps1
+++ src/arm-templates/deployment-scripts.ps1
@@ -1,29 +1,39 @@
 param(
-    [string]$Environment
+    [string]$subscriptionName,
+    [string]$env
 )
-#1 Define the suscription to use for the deployment
-Write-Host("::::::::::::::::::::::: Deployment started in $Environment :::::::::::::::::::::::")
+$environmentDisplayName = $($env.ToUpper())
 
-Write-Host("Setting account subscription for $Environment ...")
-az account set -s "Az Subscription Main"
+Write-Host
+
+Write-Host("::::::::::::::::::::::: Deployment started in $environmentDisplayName :::::::::::::::::::::::")
+
+#1 Define the suscription to use for the deployment
+Write-Host("Setting subscription '$subscriptionName' for $environmentDisplayName deployment ..")
+
+az account set -s "$subscriptionName"
 Write-Host('Subscription setted.')
+
 Write-Host
 
 #2 Deploy the resource group
-Write-Host("Deploying resource group to $Environment ...")
+Write-Host("Deploying resource group in $environmentDisplayName ..")
+
 $rgName=(az deployment sub create `
     --location "canadacentral" `
     --template-file "rg-azure-deploy.json" `
-    --parameters "rg-azure-deploy.parameters.$Environment.json" | ConvertFrom-Json).properties.parameters.rgName
+    --parameters "rg-azure-deploy.parameters.$env.json" | ConvertFrom-Json).properties.parameters.rgName
 Write-Host('Resource group deployed.')
+
 Write-Host
 
 #3 Deploy resources
-Write-Host("Deploying resources to $Environment environment ...")
+Write-Host("Deploying resources in $environmentDisplayName ..")
+
 az deployment group create `
     --resource-group $rgName.value `
     --template-file "azure-deploy.json" `
-    --parameters "azure-deploy.parameters.$Environment.json"
+    --parameters "azure-deploy.parameters.$env.json"
 
-##TODO Put this message in an if statement
-Write-Host("::::::::::::::::::::::: Successfully deployed resources to $Environment :::::::::::::::::::::::")
+##TODO ensured deployment has succeed before
+Write-Host("::::::::::::::::::::::: Successfully deployed resources in $environmentDisplayName :::::::::::::::::::::::")
